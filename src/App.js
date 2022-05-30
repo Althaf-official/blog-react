f95872caa4ac3ee267cdb9ec99700f37e6d99b9e
Althaf Sharafudheen <althaf.sharafudheen@outlook.com>
2022-05-31 00:44:55 +0400

    import login and create page render route for them also

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import {BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home/> }/>
+        <Route path="/createpost" element={<CreatePost/> }/>
+        <Route path="/login" element={<Login/> }/>
       </Routes>
     </Router>
   );
