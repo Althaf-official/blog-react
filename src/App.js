8fafcf5fa8065636258dc4b77263060561e5cec5
Althaf Sharafudheen <althaf.sharafudheen@outlook.com>
2022-05-31 01:08:57 +0400

    create link for 3 pages as a navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route,Link } from "react-router-dom";
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
 import Login from "./pages/Login";
@@ -6,6 +6,11 @@ import Login from "./pages/Login";
 function App() {
   return (
     <Router>
+      <nav>
+        <Link to="/"> Home</Link>
+        <Link to="/createpost"> Create Post</Link>
+        <Link to="/login"> login</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Home/> }/>
         <Route path="/createpost" element={<CreatePost/> }/>
