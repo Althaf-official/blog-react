c60e641fdffc3bdb38f48b64a3c5a28ad447aa23
Althaf Sharafudheen <althaf.sharafudheen@outlook.com>
2022-05-31 01:15:16 +0400

    succesfully added app.css file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import './App.css'
 import {BrowserRouter as Router, Routes, Route,Link } from "react-router-dom";
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
 import Login from "./pages/Login";
+
 
 function App() {
   return (
