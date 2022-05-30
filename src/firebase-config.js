586330c3d98e68f517dfed18de0dfd5c08bf05f3
Althaf Sharafudheen <althaf.sharafudheen@outlook.com>
2022-05-31 00:22:28 +0400

    export the auth and provider

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -19,4 +19,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+
  
