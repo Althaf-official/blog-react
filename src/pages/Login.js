05137a34b346c8d890f7c229fc2ff635b44c9ac4
Althaf Sharafudheen <althaf.sharafudheen@outlook.com>
2022-05-31 18:14:22 +0400

    Login - i pass the variable  navigate("/") .so if i lgged in i will redirected to the home page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,12 +5,13 @@ import {useNavigate } from "react-router-dom"
 
 function Login({setIsAuth}) {
     let navigate =useNavigate()
-    
+
     const signInWithGoogle = () => {
         //i call the imported function signInWithGoogle and i pass the auth and provider wich is i created in firebase-config.js file
         signInWithPopup(auth, provider).then((result) => { //the user data will be in the result.
             localStorage.setItem("isAuth", true);
             setIsAuth(true);
+            navigate("/")
         });
     };
 return (
