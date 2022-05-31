59fd7f084cf39ff2dc10352432649a58c4420a1b
Althaf Sharafudheen <althaf.sharafudheen@outlook.com>
2022-05-31 06:10:49 +0400

    Login pass the user data in result

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,22 +1,23 @@
 import React from 'react'
-import {auth,provider } from "../firebase-config"
-import {signInWithPopup} from "firebase/auth"
+import { auth, provider } from "../firebase-config"
+import { signInWithPopup } from "firebase/auth"
 
 function Login() {
+    const signInWithGoogle = () => {
+        //i call the imported function signInWithGoogle and i pass the auth and provider wich is i created in firebase-config.js file
+        signInWithPopup(auth, provider).then((result) => { //the user data will be in the result.
 
-
-    const signInWithGoogle = () =>{
-
+        })
     }
 
-
-  return (
+}
+return (
     <div className='loginPage'>
         <p>Sign In With Google</p>
         <button className='login-with-google-btn'>Sign In With Google</button>
-      
+
     </div>
-  )
+)
 }
 
 export default Login
