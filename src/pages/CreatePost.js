09d3ecb7e5bb525e8ce9ef58165c8144b21203be
Althaf Sharafudheen <althaf.sharafudheen@outlook.com>
2022-05-31 20:32:28 +0400

    CreatePost - declare useState

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function CreatePost() {
+    const [title,setTitle]= useState("")
+    const [postText,setPostText]= useState("")
+    //("")   means pass the value as a string
+
+
     return (
         <div className='createPostPage'>
             <div className='cpContainer'>
