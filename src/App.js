ef2e54813361f2ad9f7073d38d21c73c3cf42f99
Althaf Sharafudheen <althaf.sharafudheen@outlook.com>
2022-05-31 06:13:07 +0400

    login -call useState - check the user is login user or not

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import {BrowserRouter as Router, Routes, Route,Link } from "react-router-dom";
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
 import Login from "./pages/Login";
+import { useState } from 'react';
 
 
 function App() {
+  const [isAuth,setIsAuth] = useState(false);
+
+
   return (
     <Router>
       <nav>
