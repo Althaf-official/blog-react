c7845221f016cabd0a4b592deac93c08a7a7e7de
Althaf Sharafudheen <althaf.sharafudheen@outlook.com>
2022-06-01 08:38:35 +0400

    Home - succesfully showing mutiple posts

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,13 +9,17 @@ function Home() {
     useEffect(() => {//i will use useEffect for showing the data async
         const getPosts = async () => {
             const data = await getDocs(postsCollectionRef)
-            setPostList(data.docs.map((doc)=>({...doc.data(), id:doc.id})));
+            setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
         }
         getPosts()
     })
     return (
-        <div className='homePage'>{postLists.map((post) =>{// {postLists.map((post) - postList is grab all the document from collection.  and .map -for catch each one of them 
-            return <div className='post'>{post.title}</div>//in the map(post) is holding each one data
+        <div className='homePage'>{postLists.map((post) => {// {postLists.map((post) - postList is grab all the document from collection.  and .map -for catch each one of them 
+            return <div className='post'>
+                <div className='postHeader'>
+                    <div className='title'><h1>{post.title}</h1></div>
+                </div>
+            </div>//in the map(post) is holding each one data
 
         })}</div>
     )
