cf729921062880026854fdeb9b2736417b846ec2
Althaf Sharafudheen <althaf.sharafudheen@outlook.com>
2022-06-01 08:00:30 +0400

    Home- pass the referance for the collection

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react'
-import {} from 'firebase/firestore'
+import {getDocs} from 'firebase/firestore'
 function Home() {
     //~Now i want to list all the post in the database
     const [postLists,setPostList] =useState([])//initialize as a empty array
+    const postsCollectionRef = collection(db, "posts")
 
     useEffect(()=>{//i will use useEffect for showing the data async
         const getPosts =async()=>{
-            const data = await
+            const data = await getDocs(postsCollectionRef)
         }
     })
   return (
