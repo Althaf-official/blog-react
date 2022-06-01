41d38ae15ac75be345d153a083169a4b51e3b029
Althaf Sharafudheen <althaf.sharafudheen@outlook.com>
2022-06-01 06:33:50 +0400

    Home: import getDoc for collect the data from the database

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,14 @@
-import React, { useState } from 'react'
-
+import React, { useEffect, useState } from 'react'
+import {} from 'firebase/firestore'
 function Home() {
     //~Now i want to list all the post in the database
     const [postLists,setPostList] =useState([])//initialize as a empty array
+
+    useEffect(()=>{//i will use useEffect for showing the data async
+        const getPosts =async()=>{
+            const data = await
+        }
+    })
   return (
     <div className='homePage'></div>
   )
