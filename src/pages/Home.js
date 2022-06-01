d7c27b2ec6de6301b7cc8b3d666248a6febc2418
Althaf Sharafudheen <althaf.sharafudheen@outlook.com>
2022-06-01 08:16:20 +0400

    Home- console the document we want using map

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,21 @@
 import React, { useEffect, useState } from 'react'
-import {getDocs,collection} from 'firebase/firestore'
-import {db} from '../firebase-config'
+import { getDocs, collection } from 'firebase/firestore'
+import { db } from '../firebase-config'
 function Home() {
     //~Now i want to list all the post in the database
-    const [postLists,setPostList] =useState([])//initialize as a empty array
+    const [postLists, setPostList] = useState([])//initialize as a empty array
     const postsCollectionRef = collection(db, "posts")
 
-    useEffect(()=>{//i will use useEffect for showing the data async
-        const getPosts =async()=>{
+    useEffect(() => {//i will use useEffect for showing the data async
+        const getPosts = async () => {
             const data = await getDocs(postsCollectionRef)
-            console.log(data);
+            console.log(data.docs.map((doc)=>({...doc.data(), id:doc.id})));
         }
+        getPosts()
     })
-  return (
-    <div className='homePage'></div>
-  )
+    return (
+        <div className='homePage'></div>
+    )
 }
 
 export default Home
